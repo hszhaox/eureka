9d5f34042054e15beef6b78afb22422cc879a8e2
hszhaox <hszhaox@163.com>
2019-12-17 11:31:44 +0800

    Initial commit

---
+++ src/test/java/com/hszhaox/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hszhaox.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaApplicationTests {
+
+    @Test
+    public void test() {
+    }
+
+}
